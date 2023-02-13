87ad1b6c188eb7b9d16a4a86a6f912477fc75e5c
Kenji Kawaida <kenjikawaidav@gmail.com>
2023-02-13 18:08:12 -0400

    Initial commit

---
+++ src/main/java/com/soe/postgraduateprogram/app/PostgraduateProgramApplication.java
@@ -0,0 +1,13 @@
+package com.soe.postgraduateprogram.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgraduateProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgraduateProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soe/postgraduateprogram/PostgraduateProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soe.postgraduateprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgraduateProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
